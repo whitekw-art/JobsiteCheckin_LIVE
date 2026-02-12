d1f8d0e198ff930b69e1fe455e1e7e22fb6c217a
Keith <whitekw@yahoo.com>
2026-02-12 10:49:05 -0600

    Add worker check-in gallery feature

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -46,7 +46,18 @@ export default function Navigation() {
               >
                 Check-In
               </Link>
-              
+
+              <Link
+                href="/my-jobs"
+                className={`px-3 py-2 rounded-md text-sm font-medium ${
+                  pathname === '/my-jobs'
+                    ? 'bg-blue-100 text-blue-700'
+                    : 'text-gray-500 hover:text-gray-700'
+                }`}
+              >
+                My Jobs
+              </Link>
+
               {canAccessDashboard && (
                 <Link
                   href="/dashboard"
