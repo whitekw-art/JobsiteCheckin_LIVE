6bbb454954b01cdd15322070c2a0b04706d5b8d8
Keith <whitekw@yahoo.com>
2026-04-21 17:02:39 -0500

    fix: SUPER_ADMIN sees all nav tabs

--- components/DashboardShell.tsx
+++ components/DashboardShell.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+
 import { useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
